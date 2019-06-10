90dc3481a07c48db09d1ddcda7418c7248793329
Illia Khrypunov <illia.khrypunov@nure.ua>
2019-06-10 13:10:26 +0300

    Weather fixed

--- DayBook/Controller/ChatViewController.swift
+++ DayBook/Controller/ChatViewController.swift
@@ -1,22 +1,23 @@
 //
-//  MailsViewController.swift
+//  ChatViewController.swift
 //  DayBook
 //
-//  Created by Илья on 6/9/19.
+//  Created by Илья on 6/10/19.
 //  Copyright © 2019 Илья. All rights reserved.
 //
 
 import UIKit
 import GoogleSignIn
 
-class MailsViewController: UIViewController {
+class ChatViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func logOut(_ sender: Any) {
         GIDSignIn.sharedInstance()?.signOut()
         dismiss(animated: true, completion: nil)
     }
+
 }
--- DayBook/Controller/WeatherViewController.swift
+++ DayBook/Controller/WeatherViewController.swift
@@ -8,28 +8,28 @@
 
 import UIKit
 import CoreLocation
+import Alamofire
+import SwiftyJSON
 
 class WeatherViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet private weak var background: UIView!
     @IBOutlet private weak var locationLabel: UILabel!
     @IBOutlet private weak var weekdayLabel: UILabel!
-    @IBOutlet private weak var ibActivityIndicator: UIActivityIndicatorView!
     @IBOutlet private weak var ibImageView: UIImageView!
     @IBOutlet private weak var weatherStateLabel: UILabel!
     @IBOutlet private weak var temperatureLabel: UILabel!
+    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
     
-    private let gradientLayer = CAGradientLayer()
     private let locationManager = CLLocationManager()
     private var lat = 11.2
     let apiKey = "76a501c8e313f7a700c7fb95cef4a2e0"
     private var lon = 123.7
-    private var weatherData: WeatherData?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        background.layer.addSublayer(gradientLayer)
-        ibActivityIndicator.startAnimating()
+        activityIndicator.color = .black
+        activityIndicator.startAnimating()
         locationManager.requestWhenInUseAuthorization()
         if(CLLocationManager.locationServicesEnabled()){
             locationManager.delegate = self
@@ -42,37 +42,32 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
         let location = locations[0]
         lat = location.coordinate.latitude
         lon = location.coordinate.longitude
-        WeatherManager.instance.getWeatherData(latitude: lat, longitude: lon)
-        let weatherData = WeatherManager.instance.weatherData
-        locationLabel.text = weatherData.locationName
-        ibImageView.image = UIImage(named: weatherData.iconName)
-        weatherStateLabel.text = weatherData.weatherState
-        temperatureLabel.text = "\(weatherData.temperature)"
-        let date = Date()
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "EEEE"
-        self.weekdayLabel.text = dateFormatter.string(from: date)
-        let suffix = weatherData.iconName.suffix(1)
-        if(suffix=="n"){
-            self.setGreyGradientBackground()
+        Alamofire.request("http://api.openweathermap.org/data/2.5/weather?lat=\(String(describing: self.lat))&lon=\(String(describing: self.lon))&appid=\(String(describing: self.apiKey))&units=metric").responseJSON { [weak self] (response) in
+            self?.activityIndicator.stopAnimating()
+            if let responseStr = response.result.value{
+                let jsonResponse = JSON(responseStr)
+                let jsonWeather = jsonResponse["weather"].array![0]
+                let jsonTemp = jsonResponse["main"]
+                let iconName = jsonWeather["icon"].stringValue
+                let locationName = jsonResponse["name"].stringValue
+                let weatherState = jsonWeather["main"].stringValue
+                let temperature = Int(round(jsonTemp["temp"].doubleValue))
+                self?.locationLabel.text = locationName
+                self?.ibImageView.image = UIImage(named: iconName)
+                self?.weatherStateLabel.text = weatherState
+                self?.temperatureLabel.text = "\(String(describing: temperature))"
+                let date = Date()
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "EEEE"
+                self?.weekdayLabel.text = dateFormatter.string(from: date)
+                let suffix = iconName.suffix(1)
+                if(suffix=="n"){
+                    self?.background.backgroundColor = UIColor(red: 86.0/255.0, green: 61.0/255.0, blue: 111.0/255.0, alpha: 1.0)
+                }
+                else{
+                    self?.background.backgroundColor = UIColor(red: 49.0/255.0, green: 146.0/255.0, blue: 165.0/255.0, alpha: 1.0)
+                }
+            }
         }
-        else{
-            self.setBlueGradientBackground()
-        }
-        ibActivityIndicator.stopAnimating()
-    }
-    
-    func setBlueGradientBackground(){
-        let topColor = UIColor(red: 95.0/255.0, green: 165.0/255.0, blue: 1.0, alpha: 1.0).cgColor
-        let bottomColor = UIColor(red: 72.0/255.0, green: 114.0/255.0, blue: 184.0/255.0, alpha: 1.0).cgColor
-        gradientLayer.frame = view.bounds
-        gradientLayer.colors = [topColor, bottomColor]
-    }
-    
-    func setGreyGradientBackground(){
-        let topColor = UIColor(red: 151.0/255.0, green: 151.0/255.0, blue: 151.0/255.0, alpha: 1.0).cgColor
-        let bottomColor = UIColor(red: 72.0/255.0, green: 72.0/255.0, blue: 72.0/255.0, alpha: 1.0).cgColor
-        gradientLayer.frame = view.bounds
-        gradientLayer.colors = [topColor, bottomColor]
     }
 }
